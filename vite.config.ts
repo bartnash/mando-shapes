dea22c82550245c198b35abb0cef07e41b5b20fc
Bart Nash <bart@bkonash.com>
2025-08-23 07:44:21 -0500

    Changes for github pages publish.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mando-shapes/', // Added for GitHub Pages deployment
 })
